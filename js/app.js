6359bf0f387af4ec01bab8ad85d21a38f634090f
José Evamberto <evamberto.lima@gmail.com>
2019-06-17 01:09:45 -0300

    First Version

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 
-var score;
+var score=0;
+var recorde=0;
 // Inimigos que o jogador deve evitar
 var Enemy = function(x,y) {
     // Variáveis aplicadas a cada instância devem ser declaradas aqui
@@ -8,22 +9,53 @@ var Enemy = function(x,y) {
     // a imagem facilmente
     this.x = x;
     this.y = y;
-    this.speed =  10 + Math.random() * 300 +40;
+    this.speed =  10 + Math.random() * 100 +30;
     this.sprite = 'images/inseto.png';
 };
 
+
 // Atualiza a posição de um inimigo
 // Parâmetro: dt, um intervalo de tempo entre atualizações
+var colide=50;
+
 Enemy.prototype.update = function(dt) {
     // Você deve multiplicar cada movimento por dt
     // assegura que o jogo rode na mesma velocidade em todos computadores.
+    
     if (this.x < 500) {
         this.x += (dt) * this.speed;
     } else {
-        this.x = -200;
-        this.speed = 10 + Math.random() * 100 +40;
+        this.x = -190;
+        this.speed = 10 + Math.random() * 130 +30;
     }
-};
+    
+    var colideX=Math.abs(this.x-player.x);
+    var colideY=Math.abs(this.y-player.y);
+    
+    if(colideX<=colide && colideX>0 && colideY<=colide && colideY>0){
+       
+        if(score>recorde){
+            recorde=score;
+            document.getElementById("RecordePonto").innerHTML = recorde;
+            alert("Parabéns, você quebrou o recorde, continue assim!!! ");
+
+        }else{
+            alert("Seja melhor na próxima");
+        
+        }
+       
+    
+        score=0;
+        document.getElementById("pontos").innerHTML = score;
+        player.reset();
+        
+    }
+
+    
+
+
+}
+
 
 // Desenha o inimigo na tela
 Enemy.prototype.render = function() {
@@ -44,13 +76,18 @@ class Player{
     update(){
         if(this.y < 0){
             score++;
-            console.log(score);
-
+     
+            document.getElementById("pontos").innerHTML = score;
             this.reset();
         }
+
+    
+
+
     }
     render(){
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
     }
 
   
@@ -88,12 +125,14 @@ class Player{
 // Coloque todos os objetos inimigos em um array chamado allEnemies
 // Coloque o jogador em uma variável chamada player
 var allEnemies = [
-    new Enemy(-202, 60),
-    new Enemy(-210, 220),
-    new Enemy(-210, 150),
+    new Enemy(-252, 60),
+    new Enemy(-190, 220),
+    new Enemy(-200, 150),
     new Enemy(-290, 159),
     new Enemy(-290, 220),
 ];
+
+
 var player= new Player();
 
 // Esta função escuta pela tecla pressionada e envia a tecla para o método
